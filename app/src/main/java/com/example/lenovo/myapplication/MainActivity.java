b70fb4d00b10ec97b6144c381ea9d5adfa6c8c92
Max Oude Spraakste <max@maxoudespraakste.nl>
2017-05-11 14:27:14 +0200

    Merge conflict veroorzaken

--- app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lenovo.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //asdfasf
+        Log.i("yay", "hij werkt");
+        boolean hasMergeConflict = true;
+        if (hasMergeConflict) Log.i("noo", "fuck");
     }
 }
